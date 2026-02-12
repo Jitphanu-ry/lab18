f952645559640eafb60749f82e38b605b24f7a34
Jitphanu-ry <jitphanuriyasarn@gmail.com>
2026-02-12 20:35:33 +0700

    up1

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -5,8 +5,22 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *R1,Rect *R2){
+	double x1 = max(R1->x,R2->x);
+	double x2 = min(R1->x + R1->w,R2->x + R2->w);
+	double y1;
+	if(R2->y > R1->y){
+	    y1 = max(R1->y,R2->y);
+	    }else{
+	    y1 = min(R1->y,R2->y);
+	}
+	double y2 = min(R1->y + R1->h,R2->y + R2->h);
+	double length = x2 - x1;
+	double height = y2 - y1;
+	
+	if(length <= 0 ||height <= 0){
+	    return 0;
+	}
+	
+	return length * height;
 }
